1c3e9519e8f7b770274ec4a26bf53c301f88284c
CarvedCoder <CarvedCoder@users.noreply.github.com>
2026-01-26 04:07:17 +0530

    Schema for forge

---
+++ forge/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs::{read_to_string, write};
+
+use forge::forge_schema::ForgeSchema;
+
+fn generate_toml(path: &str) {
+    let schema = ForgeSchema::default();
+    let toml_schema =
+        toml::to_string_pretty(&schema).expect("Failed to serialize forge schema to TOML");
+    write(path, toml_schema).expect("Failed to write forge.toml");
+    println!("forge.toml produced successfully");
+}
+
+fn load_toml(path: &str) {
+    let read_toml = read_to_string(path).expect("Failed to read forge.toml");
+    let config: ForgeSchema = toml::from_str(&read_toml).expect("Failed to parse forge.toml");
+    println!("{:#?}", config);
+}
+
+fn main() {
+    generate_toml("forge.toml");
+    load_toml("forge.toml");
+}
